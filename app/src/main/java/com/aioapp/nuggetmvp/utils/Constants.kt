625fbf9150354c7907236f2696d299a587952ac1
IrumZareen <irum.zareen@aioapp.com>
2024-07-04 14:42:30 +0500

    New tokens for picovoice

--- app/src/main/java/com/aioapp/nuggetmvp/utils/Constants.kt
+++ app/src/main/java/com/aioapp/nuggetmvp/utils/Constants.kt
@@ -6,7 +6,10 @@ object Constants {
     var BASE_URL_TEXT_TO_RESPONSE = "http://44.231.228.32:8005/"
     var BASE_URL_Refill = "http://44.231.228.32:8006/"
 //    var ACCESS_KEY_PORCUPINE = "wbY3zsAJP9AcYpSUsX9kR12QKHmKrvD+c4+CZ08ZX+o0SONvgSOTHw=="
-    var ACCESS_KEY_PORCUPINE = "hrj+4m71rdDnHyuh7vaUPK7HdfAO6pkLv0ZaRpMqkBKimjCeHAHmlA=="
+//    var ACCESS_KEY_PORCUPINE = "hrj+4m71rdDnHyuh7vaUPK7HdfAO6pkLv0ZaRpMqkBKimjCeHAHmlA==" //irumzareen
+//    var ACCESS_KEY_PORCUPINE = "+HNTGs5jgOJ5xrac5vEiq/AvwOSeaFVk28w+FPcyNVWj6lko1ECMRQ==" //umair
+//    var ACCESS_KEY_PORCUPINE = "Fu66yvCi94100YzbgpXinQm00uukQTov5IeHELs34H2b6kZinlHbgw==" //fakhir's
+    var ACCESS_KEY_PORCUPINE = "Fu66yvCi94100YzbgpXinQm00uukQTov5IeHELs34H2b6kZinlHbgw==" //Harras'
     const val CHANNEL_ID = "screen_shoot_11"
     const val CHANNEL_NAME = "screen_shoot"
     const val TAG_NUGET_MVP = "TAG_NUGET_MVP"
